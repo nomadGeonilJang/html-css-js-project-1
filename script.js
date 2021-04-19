e33759e6bde17408a322f57b8f0f7a5ea42722b4
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-04-19 23:47:08 +0900

    update style and js

---
+++ script.js
@@ -0,0 +1,5 @@
+const icons = document.querySelectorAll(".section-1-icons i")
+setInterval(()=>{
+    const icon = document.querySelector(".section-1-icons .change")
+    icon.classList.remove("change")
+},4000)
